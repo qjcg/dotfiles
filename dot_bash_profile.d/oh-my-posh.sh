cea79fc1babffafe8529dcf6b205802bc4991b21
John Gosset <john@gossetx.com>
2024-11-14 10:30:00 -0500

    chore: Bump OMP version

--- dot_bash_profile.d/oh-my-posh.sh
+++ dot_bash_profile.d/oh-my-posh.sh
@@ -1,4 +1,4 @@
 hash oh-my-posh 2>/dev/null || return
 
 # FIXME: No effect
-#eval "$(oh-my-posh init bash --config https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/negligible.omp.json)"
+eval "$(oh-my-posh init bash --config https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/negligible.omp.json)"
