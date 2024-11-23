8eb43d39ce412dc3659ce5491aaa687acaa251a1
John Gosset <john@gossetx.com>
2024-11-22 21:00:01 -0500

    feat: Add early-init startup-speed-annoyance-suppression settings

--- private_dot_config/private_emacs/early-init.el
+++ private_dot_config/private_emacs/early-init.el
@@ -1,2 +1,8 @@
 ;; '(fullscreen . maximized) or '(fullscreen . fullscreen)
 (add-to-list 'initial-frame-alist '(fullscreen . fullscreen))
+
+;; Startup speed, annoyance suppression
+(setq gc-cons-threshold 10000000)
+(setq byte-compile-warnings '(not obsolete))
+(setq warning-suppress-log-types '((comp) (bytecomp)))
+(setq native-comp-async-report-warnings-errors 'silent)
