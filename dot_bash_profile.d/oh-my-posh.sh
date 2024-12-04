07dc97994a8c0d16340aae8ada68ed8db59d650b
John Gosset <john@gossetx.com>
2024-12-04 05:17:26 -0500

    feat: Add oh-my-posh config file

--- dot_bash_profile.d/oh-my-posh.sh
+++ dot_bash_profile.d/oh-my-posh.sh
@@ -1,4 +1,3 @@
 hash oh-my-posh 2>/dev/null || return
 
-# FIXME: No effect
-eval "$(oh-my-posh init bash --config https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/negligible.omp.json)"
+eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh/jg-minimalist.yaml)"
