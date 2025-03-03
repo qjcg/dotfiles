df4793607dc7b355c7859a18e679f21aa59a542d
John Gosset <john@gossetx.com>
2025-03-03 11:17:41 -0500

    fix: Only add GEM_HOME to PATH if configured

--- dot_bash_profile.d/10-ruby.sh
+++ dot_bash_profile.d/10-ruby.sh
@@ -1,4 +1,6 @@
 hash gem 2>/dev/null || return
 
-export GEM_HOME="$(gem env user_gemhome)"
-export PATH="$PATH:$GEM_HOME/bin"
+gem env user_gemhome 2>/dev/null && {
+    export GEM_HOME="$(gem env user_gemhome)"
+    export PATH="$PATH:$GEM_HOME/bin"
+}
