449d47e4258e57f710f0860651d899e1f6690412
John Gosset <john@gossetx.com>
2025-03-07 11:13:37 -0500

    fix(ruby): Do not output path when GEM_HOME is set

--- dot_bash_profile.d/10-ruby.sh
+++ dot_bash_profile.d/10-ruby.sh
@@ -1,6 +1,6 @@
 hash gem 2>/dev/null || return
 
-gem env user_gemhome 2>/dev/null && {
+gem env user_gemhome &>/dev/null && {
     export GEM_HOME="$(gem env user_gemhome)"
     export PATH="$PATH:$GEM_HOME/bin"
 }
