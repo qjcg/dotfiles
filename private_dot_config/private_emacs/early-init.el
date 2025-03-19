203db3970bb8d8de5f76a2f70749bd46565a4cd2
John Gosset <john@gossetx.com>
2025-03-19 05:14:22 -0400

    fix: Revise emacs package loading

--- private_dot_config/private_emacs/early-init.el
+++ private_dot_config/private_emacs/early-init.el
@@ -1,8 +1,8 @@
 ;; Startup speed, annoyance suppression
-(setq gc-cons-threshold 10000000)
+(setopt gc-cons-threshold 10000000)
 (setq byte-compile-warnings '(not obsolete))
-(setq warning-suppress-log-types '((comp) (bytecomp)))
-(setq native-comp-async-report-warnings-errors 'silent)
+(setopt warning-suppress-log-types '((comp) (bytecomp)))
+(setopt native-comp-async-report-warnings-errors 'silent)
 
 ;; '(fullscreen . maximized) or '(fullscreen . fullscreen)
 (add-to-list 'initial-frame-alist '(fullscreen . fullscreen))
@@ -10,8 +10,6 @@
 ;; Add personal lisp dir to load-path.
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-;; Use no-littering dir for native compilation cache.
-;; https://github.com/emacscollective/no-littering#native-compilation-cache
 (require 'xdg)
 (when (fboundp 'startup-redirect-eln-cache)
   (startup-redirect-eln-cache
