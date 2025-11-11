f8b44bf1706fbece02636ef6abec310448022fed
John Gosset <john@gossetx.com>
2025-11-11 14:13:15 -0500

    fix: Add ~/.cargo/bin to PATH before testing whether bins exist

--- dot_bash_profile.d/10-rust.sh
+++ dot_bash_profile.d/10-rust.sh
@@ -1,6 +1,6 @@
-hash cargo 2>/dev/null || return
-
 idempotent_add_to_path prepend ~/.cargo/bin
+
+hash cargo 2>/dev/null || return
 
 eval "$(rustup completions bash cargo)"
 eval "$(rustup completions bash rustup)"
