616be74d973d3556193d5b492a20a5cec98d2f86
John Gosset <john@gossetx.com>
2025-11-15 13:16:58 -0500

    chore: Add .dir-locals.el in oh-my-posh dir

---
+++ private_dot_config/oh-my-posh/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((cue-mode . ((cue-eval-command . ("cue" "eval" "-c" ".")))))
