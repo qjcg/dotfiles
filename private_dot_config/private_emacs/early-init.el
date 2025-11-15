8691057f6e4937d70569fcfed434753416c4ad71
John Gosset <john@gossetx.com>
2025-11-15 16:22:26 -0500

    chore(emacs): Tweak and simplify early-init.el

--- private_dot_config/private_emacs/early-init.el
+++ private_dot_config/private_emacs/early-init.el
@@ -4,25 +4,26 @@
 (setopt warning-suppress-log-types '((comp) (bytecomp)))
 (setopt native-comp-async-report-warnings-errors 'silent)
 
-(add-to-list 'initial-frame-alist '(fullscreen . fullscreen))
-
-(setopt user-emacs-directory "~/.config/emacs")
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
+;; ELN Cache
 (require 'xdg) ;; FIXME: handle package not installed
 (when (fboundp 'startup-redirect-eln-cache)
   (startup-redirect-eln-cache
    (convert-standard-filename
     (expand-file-name  "emacs/eln-cache" (xdg-cache-home)))))
 
+;; Package Loading
 ;; The lines below fix an issue where some packages in load-path were
 ;; sourced from from ~/.emacs.d/elpa, and others from
 ;; ~/.config/emacs/var/elpa (i.e. seemingly a race between package.el and
 ;; no-littering.el at startup).
 (setq package-enable-at-startup nil)
+(setopt user-emacs-directory "~/.config/emacs")
 (setq package-user-dir (expand-file-name "var/elpa" user-emacs-directory))
 (setq no-littering-etc-directory (expand-file-name "etc/" user-emacs-directory))
 (setq no-littering-var-directory (expand-file-name "var/" user-emacs-directory))
+
+;; Fullscreen
+(add-to-list 'initial-frame-alist '(fullscreen . fullscreen))
 
 (when init-file-debug
   (message "early-init.el loaded!"))
